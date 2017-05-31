90dded34f0fbb1033a113d9655ff8927d20c4359
Andrei Hodecker <Andrei Hodecker>
2017-05-31 01:51:52 +0100

    prototipando um design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Assts!';
+  title = 'Assts';
   description = 'A free and open source digital asset tracker.'
 }
