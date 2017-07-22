c4338e0f52d20e2eaee91a6f3228061c1901b9d5
Nauro <nauro@outlook.com>
2017-07-21 22:04:52 -0300

    Adding authorization (jwt token); Adding login; Adding buttons to logout / getUser; When creating new user, do the login;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AuthenticationService } from './authentication.service';
+import { UserService } from './user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -10,19 +12,21 @@ import { AssetService } from './asset.service';
 
 import { CoinService } from './coin.service';
 import { ExchangeService } from './exchange.service';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AssetListComponent,
-    AssetFormComponent
+    AssetFormComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [AssetService, CoinService, ExchangeService],
+  providers: [AssetService, CoinService, ExchangeService, UserService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user.service.ts
@@ -0,0 +1,54 @@
+import { AuthenticationService } from './authentication.service';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { User } from './user';
+
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+@Injectable()
+export class UserService {
+
+  //FORGIVE ME OH ALMIGHTY GOD FOR THIS. IT'S JUST A TEST, I PROMISE
+  baseUrl = "https://getcryptofolio.com/";
+
+  constructor(private http: Http, private authenticationService : AuthenticationService) { }
+
+  signup(user: User): Observable<User> {
+    return this.http.post(`${this.baseUrl}api/auth/signup`,
+      JSON.stringify({ user })
+      , { headers: this.getHeaders() })
+      .map(res => { return res.json() })
+      .catch(this.handleError);
+  }
+
+  
+
+  update(user: User) {
+    return this.http.put(`${this.baseUrl}api/users/me`,
+      JSON.stringify({ user })
+      , { headers: this.getHeaders() });
+  }
+
+  get() {
+    return this.http.get(`${this.baseUrl}api/users/me`
+      , { headers: this.getHeaders() });
+  }
+
+  private getHeaders() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization',`Bearer ${this.authenticationService.jwt}`);
+    return headers;
+  }
+
+
+
+  private handleError(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
+
+
+
+}
