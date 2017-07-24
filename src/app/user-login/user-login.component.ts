48c24370ea348a0e9656fdc16570322379a0a789
Nauro <nauro@outlook.com>
2017-07-24 01:49:24 -0300

    Cleaning up the code - Adding login methods to user-login; - Adding sign-up methods to user-signup;

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -19,50 +19,10 @@ export class UserLoginComponent implements OnInit {
   }
 
   onSubmit(form) {
-    if (this.creating) {
-      if (this.user.password !== this.repeatPassword) {
-
-      }
-
-      this.userService.signup(this.user).subscribe(
-        result => {
-          this.authenticationService.login(this.user.username, this.user.password).subscribe(
-            rev => {
-              console.log(rev)
-            }
-          )
-        }
-      );
-
-    } else {
-      let usernameEmail = (this.user.username) ? this.user.username : this.user.email;
-      this.authenticationService.login(usernameEmail, this.user.password).subscribe(
-        result => console.log(result)
-      )
-    }
-  }
-
-  login() {
-
-  }
-
-  signup() {
-
-  }
-
-  create() {
-    this.creating = !this.creating;
-  }
-
-  getMe() {
-    this.userService.get().subscribe(
+    let usernameEmail = (this.user.username) ? this.user.username : this.user.email;
+    this.authenticationService.login(usernameEmail, this.user.password).subscribe(
       result => console.log(result)
     )
   }
-
-  logout() {
-    this.authenticationService.logout();
-  }
-
 
 }
--- src/app/user-signup/user-signup.component.ts
+++ src/app/user-signup/user-signup.component.ts
@@ -12,57 +12,25 @@ export class UserSignupComponent implements OnInit {
 
   user: User = new User();
   repeatPassword: string;
-  creating: boolean = false;
 
   constructor(private userService: UserService, private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
 
-  onSubmit(form) {
-    if (this.creating) {
-      if (this.user.password !== this.repeatPassword) {
-
-      }
-
-      this.userService.signup(this.user).subscribe(
-        result => {
-          this.authenticationService.login(this.user.username, this.user.password).subscribe(
-            rev => {
-              console.log(rev)
-            }
-          )
-        }
-      );
-
-    } else {
-      let usernameEmail = (this.user.username) ? this.user.username : this.user.email;
-      this.authenticationService.login(usernameEmail, this.user.password).subscribe(
-        result => console.log(result)
-      )
+  signup(form) {
+    if (this.user.password !== this.repeatPassword) {
     }
+    this.userService.signup(this.user).subscribe(
+      result => {
+        this.authenticationService.login(this.user.username, this.user.password).subscribe(
+          rev => {
+            console.log(rev)
+          }
+        )
+      }
+    );
   }
 
-  login() {
-
-  }
-
-  signup() {
-
-  }
-
-  create() {
-    this.creating = !this.creating;
-  }
-
-  getMe() {
-    this.userService.get().subscribe(
-      result => console.log(result)
-    )
-  }
-
-  logout() {
-    this.authenticationService.logout();
-  }
 
 }
